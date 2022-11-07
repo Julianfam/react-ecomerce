1136f2ea8d0dca352709c9b09136072e53580cf9
Julian Andrade <julifeli123@gmail.com>
2022-11-07 15:10:04 -0500

    modificamos el index a react 18, se conecto seccion productlist con API, se configuro babel para ejecucion correcta

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import App from './routes/App';
 
 
-ReactDOM.render(<App />,document.getElementById('app'));
+// ReactDOM.render(<)App />,document.getElementById('app'));
 
 
 
 
 
-
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App tab= "home" />)
